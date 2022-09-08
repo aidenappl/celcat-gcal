6b83b9a4c307ca539df7e4be94f976e538495763
Aiden Appleby <aiden.appleby3@gmail.com>
2022-09-08 18:33:13 +0100

    changes to main handler

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.18
 require (
 	github.com/arran4/golang-ical v0.0.0-20220517104411-fd89fefb0182
 	github.com/google/uuid v1.3.0
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 )
+
+require github.com/felixge/httpsnoop v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/aidenappl/celcat-gcal/routers"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+
+	// AWS Healthcheck Handler
+	r.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods("GET", "POST", "OPTIONS")
+
+	// Get Calendar Method
 	r.HandleFunc("/getCalendar", routers.HandleGetCalendar).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8000", r)
+	// Host & Serve the API
+
+	// Launch API Listener
+	fmt.Printf("✅ APLB Celcat Gcal Service running on port %s\n", "8000")
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Origin", "Authorization", "Accept", "X-CSRF-Token"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
 }
